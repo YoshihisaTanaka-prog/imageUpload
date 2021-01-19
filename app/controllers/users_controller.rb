0ac936c26cfc723ba2c94b6676a4d369d36cd12f
YoshihisaTanaka-prog <72920523+YoshihisaTanaka-prog@users.noreply.github.com>
2021-01-19 16:52:44 +0900

    6th commit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,40 @@
 class UsersController < ApplicationController
+
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
+
   def index
-    @user = User.new  #form用インスタンス
     @users = User.all #ユーザー一覧表示用
+    #render :json => @users
+  end
+
+  def new
+    @user = User.new
   end
 
   def create
     @user = User.new(user_params)
     @user.save
     redirect_to root_url
+  end
+
+  def show  
+  end
+
+  def edit
+  end
+
+  def update
+    @user.update(user_params)
+    redirect_to root_url
+  end
+
+  def destroy
+    @user.destroy
+    redirect_to root_url
+  end
+
+  def set_user
+    @user = User.find(params['id'])
   end
   
   private 
--- app/uploaders/user_uploader.rb
+++ app/uploaders/user_uploader.rb
@@ -2,6 +2,6 @@ class UserUploader < CarrierWave::Uploader::Base
   include Cloudinary::CarrierWave
 
   # 最大サイズを指定
-  process resize_to_fit: [300, 200]
+  #process resize_to_fit: [300, 200]
 
 end
